2544474e99f5f98fe4f96f249f2a373cb605a145
oackland <oackland.toro@outlook.com>
2023-08-21 09:06:15 -0600

    initial

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,32 @@
-from flask import Blueprint, render_template, request
 import requests
+from flask import Blueprint, render_template, request
 
 bp = Blueprint("pokemon", __name__)
 
 
-@bp.route("/")
-def index():
-	return render_template("index.html", pokemon_data=None, error_message=None)
+@bp.route('/')
+def home():
+	return render_template('index.html')
+
+
+@bp.route("/projects")
+def projects():
+	return render_template("projects.html")
+
+
+@bp.route("/contact")
+def contacts():
+	return render_template("contacts.html")
+
+
+@bp.route("/signup")
+def signup():
+	return render_template("signup.html")
+
+
+@bp.route("/game")
+def game():
+	return render_template("game.html", pokemon_data=None, error_message=None)
 
 
 @bp.route("/fetch_pokemon_data", methods=["POST"])
@@ -27,8 +47,14 @@ def fetch_pokemon_data():
 				"ability":    data["abilities"][0]["ability"]["name"]
 		}
 
-		return render_template("index.html", pokemon_data=pokemon_data, error_message=None)
+		return render_template(
+			"base.html",
+			content=render_template("pokedex.html", pokemon_data=pokemon_data, error_message=None)
+		)
 
 	except requests.exceptions.RequestException as e:
 		error_message = f"Error fetching Pokémon data: {str(e)}"
-		return render_template("index.html", pokemon_data=None, error_message=error_message)
+		return render_template(
+			"base.html",
+			content=render_template("pokedex.html", pokemon_data=None, error_message=error_message)
+		)
