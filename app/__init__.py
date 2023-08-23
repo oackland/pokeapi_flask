78f8eee1502c0665b60afefc9c93099e4a263221
oackland <oackland.toro@outlook.com>
2023-08-22 19:21:43 -0600

    tracking updates

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,36 @@
+# from dotenv import load_dotenv
+# from flask import Flask
+#
+# from config import Config
+# from .routes import bp as pokemon_bp
+#
+# app = Flask(__name__)
+# app.config.from_object(Config)
+# load_dotenv()
+# from . import routes
+#
+# app.register_blueprint(pokemon_bp)
+#
+# if __name__ == "__main__":
+# 	app.run()
 from flask import Flask
 
-from config import Config
+from config import Config  # Use relative import here
+from .models import db
 from .routes import bp as pokemon_bp
 
-app = Flask(__name__)
-app.config.from_object(Config)
-from . import routes
 
-app.register_blueprint(pokemon_bp)
+def create_app():
+	app = Flask(__name__)
+	app.config.from_object(Config)
+	db.init_app(app)
+
+	# Register the blueprint
+	app.register_blueprint(pokemon_bp)
+
+	return app
+
 
 if __name__ == "__main__":
+	app = create_app()
 	app.run()
--- config.py
+++ config.py
@@ -1,11 +1,19 @@
 import os
 
+from dotenv import load_dotenv
 
-class Config():
-	FLASK_APP = os.environ.get('FLASK_APP')
-	FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
-	SECRET_KEY = os.environ.get('SECRET_KEY')
+load_dotenv()
 
 
-def SECRET_KEY():
-	return None
+class Config:
+	SECRET_KEY = os.getenv("SECRET_KEY")
+	FLASK_APP = os.getenv("FLASK_APP")
+	FLASK_DEBUG = os.getenv("FLASK_DEBUG")
+	FLASK_ENV = os.getenv("FLASK_ENV")
+
+	SQLALCHEMY_DATABASE_URI = (f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@"
+							   f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}")
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+	API_KEY = os.getenv("API_KEY")
+	API_SECRET = os.getenv("API_SECRET")
