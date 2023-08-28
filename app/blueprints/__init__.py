9a728f680046ab1647339ae7dca2a4d56289f586
oackland <oackland.toro@outlook.com>
2023-08-28 13:29:44 -0600

    paying for good
    Signed-off-by: oackland <oackland.toro@outlook.com>

--- app/blueprints/__init__.py
+++ app/blueprints/__init__.py
@@ -6,3 +6,7 @@
 #
 #  The above copyright notice and this permission notice shall be included in all
 #  copies or substantial portions of the Software.
+
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
